53037bdf436636c60986424340a0a8028b3b0fed
Nathan Thomas <nwthomas@me.com>
2018-12-01 00:25:47 -0800

    Make mobile layout home-page.less

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 const buttons = document.querySelectorAll("input");
 const timerText = document.getElementsByClassName("timer__text");
 
+function runPomodoro() {
+  // Stopped working here for the evening
+}
+
 (function assignBtnClicks() {
   // IIFE immediately invoked
   buttons.forEach(input => {
     input.addEventListener("click", () => {
       if (input.value === "Pomodoro") {
         runPomodoro();
+        console.log("!");
       } else if (input.value === "Break") {
         runBreak();
       } else if (input.value === "Pause") {
